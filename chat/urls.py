50febbe5e041cc3be151b540cba4440171bc9b2d
渡辺仁志 <watanabehitoshi@watanabehitoshi-no-MacBook-Pro.local>
2018-08-22 15:36:27 +0700

    append consumer.py

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     url(r'^$', views.index, name='index'),
-}
+    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+]
