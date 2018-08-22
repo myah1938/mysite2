90c44b641107ae8e48cda36f31fe7518d26e15f6
渡辺仁志 <watanabehitoshi@watanabehitoshi-no-MacBook-Pro.local>
2018-08-22 16:37:27 +0700

    just send messege but don't chat with other page

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from . import consumer 
+from . import consumers 
 
 websocket_urlpatterns = [
      url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
